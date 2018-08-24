b5bf79c1f4c49542a03bc8ee0454974fa0fefc5b
Neal Harris <neal.harris@helix.com>
2018-08-24 09:52:29 -0700

    adds one-shot to butterfly key

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -251,7 +251,7 @@ KEYMAPS(
    M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
+   OSM(RightAlt),  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
    OSM(RightShift), Key_Enter, Key_Spacebar, OSM(RightControl),
    ShiftToLayer(FUNCTION)),
 
